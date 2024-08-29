da64480a8b6f191ebfbc666ac37822da5f1011ba
AnishXD <110338891+AnishXD@users.noreply.github.com>
2024-08-29 17:44:18 +0530

    New commits constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,8 @@
-export const USER_API_END_POINT = "https://job-portal-complete.vercel.app/api/v1/user";
-export const JOB_API_END_POINT = "https://job-portal-complete.vercel.app/api/v1/job";
+export const USER_API_END_POINT = "https://job-portal-backend-sandy.vercel.app/api/v1/user";
+export const JOB_API_END_POINT = "https://job-portal-backend-sandy.vercel.app/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "https://job-portal-complete.vercel.app/api/v1/application";
-export const COMPANY_API_END_POINT = "https://job-portal-complete.vercel.app/api/v1/company";
+  "https://job-portal-backend-sandy.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT = "https://job-portal-backend-sandy.vercel.app/api/v1/company";
 
 export const defaultJobData = [
   {
